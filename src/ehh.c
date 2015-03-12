856a50cd7d04156286fb83781116fe8c78bae8d7
Ryan Layer <ryan.layer@gmail.com>
2015-03-12 15:02:35 -0400

    do up and down stream.  genetic and physical distance

--- src/ehh.c
+++ src/ehh.c
@@ -4,7 +4,8 @@
 int usage(char *prog)
 {
     fprintf(stderr, "usage:\t%s <options>\n"
-                    "\t\t-f\tfile name\n"
+                    "\t\t-f\ttped file name\n"
+                    "\t\t-m\tmap file name\n"
                     "\t\t-v\tnumber of variants\n"
                     "\t\t-s\tnumber of samples\n"
                     "\t\t-l\tlocus ID\n"
@@ -16,11 +17,12 @@ int usage(char *prog)
 
 int main(int argc, char **argv)
 {
-    char *file_name, *locus_id;
+    char *map_file_name, *tped_file_name, *locus_id;
     uint32_t num_samples, num_variants;
 
     //{{{ input params
     int s_is_set = 0,
+        m_is_set = 0,
         f_is_set = 0,
         l_is_set = 0,
         v_is_set = 0,
@@ -28,7 +30,7 @@ int main(int argc, char **argv)
 
     int c;
 
-    while ((c = getopt (argc, argv, "hs:f:v:l:")) != -1) {
+    while ((c = getopt (argc, argv, "hs:f:v:l:m:")) != -1) {
         switch (c) {
         case 'h':
             h_is_set = 1;
@@ -43,17 +45,22 @@ int main(int argc, char **argv)
             break;
         case 'f':
             f_is_set = 1;
-            file_name = optarg;
+            tped_file_name = optarg;
             break;
         case 'l':
             l_is_set = 1;
             locus_id = optarg;
             break;
+        case 'm':
+            m_is_set = 1;
+            map_file_name = optarg;
+            break;
         case '?':
             if ( (optopt == 'f') || 
                  (optopt == 'v') || 
                  (optopt == 's') || 
-                 (optopt == 'l') )
+                 (optopt == 'l') || 
+                 (optopt == 'm') )
                 fprintf (stderr, "Option -%c requires an argument.\n",
                          optopt);
             else if (isprint (optopt))
@@ -87,6 +94,12 @@ int main(int argc, char **argv)
         fprintf(stderr, "Number of samples is not set.\n");
         return usage(argv[0]);
     }
+
+    if (!m_is_set) {
+        fprintf(stderr, "Map file name not set.\n");
+        return usage(argv[0]);
+    }
+
     //}}}
 
     /*
@@ -106,6 +119,31 @@ int main(int argc, char **argv)
 
     bcf_destroy(rec);
     */
+
+    FILE *fp = fopen(map_file_name, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "ERROR: Could not open %s\n", map_file_name);
+        exit(1);
+    } 
+
+    char *p, *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    uint32_t i = 0;
+    double *genetic_pos = (double *)malloc(num_variants * sizeof(double));
+    uint32_t *physical_pos = (uint32_t *)
+            malloc(num_variants * sizeof(uint32_t));
+
+    while ( (linelen = getline(&line, &linecap, fp)) > 0 ) {
+        p = strtok(line, " "); // chr
+        p = strtok(NULL, " "); // name
+        p = strtok(NULL, " "); // genetic pos
+        genetic_pos[i] = atof(p);
+        p = strtok(NULL, " "); // physical pos
+        physical_pos[i] = atoi(p);
+        i+=1;
+    }
+    fclose(fp);
 
     uint32_t n_words = num_samples / 32 + 1;
     uint32_t n_bytes =  n_words*sizeof(uint32_t);
@@ -130,17 +168,16 @@ int main(int argc, char **argv)
 
     uint32_t *R = (uint32_t *)malloc(n_bytes);
 
-    FILE *fp = fopen(file_name, "r");
+    //FILE *fp = fopen(tped_file_name, "r");
+    fp = fopen(tped_file_name, "r");
     
     if (fp == NULL) {
-        fprintf(stderr, "ERROR: Could not open %s\n", file_name);
+        fprintf(stderr, "ERROR: Could not open %s\n", tped_file_name);
         exit(1);
     } 
 
-    char *p, *h, *line = NULL;
-    size_t linecap = 0;
-    ssize_t linelen;
-    uint32_t i, j, pop, len_A0 = 0, len_A1 = 0, len_t_A = 0, S = 0, B_i = 0;
+    char *h;
+    uint32_t j, pop, len_A0 = 0, len_A1 = 0, len_t_A = 0, S = 0, B_i = 0;
     int locus_found = 0;
 
 
@@ -156,7 +193,6 @@ int main(int argc, char **argv)
         // if we hit the locus of interest, set C0 and C1 as the masks
         // for the ancestral and derived alleles
         if (strcmp(p, locus_id) == 0) {
-            fprintf(stderr, "%s\n", p);
             set_bit_arrays(h, C0, C1, num_samples);
             num_C0 = pop_count(C0, n_words);
             num_C1 = pop_count(C1, n_words);
@@ -177,10 +213,12 @@ int main(int argc, char **argv)
     }
 
     // work down stream with just C1
-    i = B_i - 1; //move back one to start at the targe locus
+    uint32_t locus_i = B_i - 1; //move back one to start at the targe locus
+    double locus_genetic_pos = genetic_pos[locus_i];
+    uint32_t locus_physical_pos = physical_pos[locus_i];
 
-    B0 = B + (i*2*n_words);
-    B1 = B + ((i*2+1)*n_words);
+    B0 = B + (locus_i*2*n_words);
+    B1 = B + ((locus_i*2+1)*n_words);
 
     and(R, B0, C1, n_words);
     memcpy(A1, R, n_bytes);
@@ -194,145 +232,121 @@ int main(int argc, char **argv)
     memcpy(A0+n_words, R, n_bytes);
     len_A0 = 2;
 
+    i = locus_i - 1;
+    long double e0 = 1, e1 = 1;
+    //while ( (i > locus_i - 150) ) {
+    while ( (i > 0) && (e0 > 0.05) && (e1 > 0.05) ) {
+        printf("-%u\t%f\t", locus_physical_pos-physical_pos[i],
+                           genetic_pos[i]-locus_genetic_pos);
+        e1 = ehh_step(&A1,
+                      &len_A1,
+                      C1,
+                      num_C1,
+                      B,
+                      i,
+                      &S,
+                      n_words,
+                      n_bytes,
+                      B0_c,
+                      B1_c,
+                      R,
+                      &t_A);
 
-    for (i = B_i - 2; i > B_i - 12; i-=1) {
-        fprintf(stderr, "i:%u\t", i);
-
-#if 1
-        long double e1 = ehh_step(&A1,
-                                  &len_A1,
-                                  C1,
-                                  num_C1,
-                                  B,
-                                  i,
-                                  &S,
-                                  n_words,
-                                  n_bytes,
-                                  B0_c,
-                                  B1_c,
-                                  R,
-                                  &t_A);
         printf("%Lf\t", e1);
 
-        long double e0 = ehh_step(&A0,
-                                  &len_A0,
-                                  C0,
-                                  num_C0,
-                                  B,
-                                  i,
-                                  &S,
-                                  n_words,
-                                  n_bytes,
-                                  B0_c,
-                                  B1_c,
-                                  R,
-                                  &t_A);
+        e0 = ehh_step(&A0,
+                      &len_A0,
+                      C0,
+                      num_C0,
+                      B,
+                      i,
+                      &S,
+                      n_words,
+                      n_bytes,
+                      B0_c,
+                      B1_c,
+                      R,
+                      &t_A);
+
         printf("%Lf\n", e0);
-#endif
 
-#if 0
-        fprintf(stderr, "len_A:%u\t", len_A);
-        B0 = B + (i*2*n_words);
-        B1 = B + ((i*2+1)*n_words);
-
-        and(B0_c, B0, C1, n_words);
-        and(B1_c, B1, C1, n_words);
-
-        len_t_A = 0;
-        for (j = 0; j < len_A; ++j) {
-            and(R, A + (j*n_words), B0_c, n_words);
-            pop = pop_count(R, n_words);
-
-            if (pop == 1) 
-                S += 1;
-            else if (pop > 0) {
-                memcpy(t_A+len_t_A*n_words, R, n_bytes);
-                len_t_A += 1;
-            }
-
-            and(R, A + (j*n_words), B1_c, n_words);
-            pop = pop_count(R, n_words);
-
-            if (pop == 1) 
-                S += 1;
-            else if (pop > 0) {
-                memcpy(t_A+len_t_A*n_words, R, n_bytes);
-                len_t_A += 1;
-            }
-        }
-
-        len_A = len_t_A;
-        swap_A = A;
-        A = t_A;
-        t_A = swap_A;
-
-        //long double n_choose_2[100001];
-        long double ehh_d = 0;
-        for (j = 0; j < len_A; ++j) {
-            printf("%u ", pop_count(A+(j*n_words), n_words));
-            ehh_d += n_choose_2[pop_count(A+(j*n_words), n_words)] /
-                     n_choose_2[num_C1];
-        }
-        for (j = 0; j < S; ++j) {
-            printf("1 ");
-        }
-        printf(" %Lf\n", ehh_d);
-#endif
+        i -= 1;
     }
-    
+    printf("--\n");
 
-#if 0
-        if (empty == 0) {
-            empty = 1;
-            memcpy(A, B0, n_bytes);
-            memcpy(A+n_words, B1, n_bytes);
-            len_A = 2;
-        } else {
-            len_t_A = 0;
+    // work upstream
+    B0 = B + (locus_i*2*n_words);
+    B1 = B + ((locus_i*2+1)*n_words);
 
-            for (i = 0; i < len_A; ++i) {
+    and(R, B0, C1, n_words);
+    memcpy(A1, R, n_bytes);
+    and(R, B1, C1, n_words);
+    memcpy(A1+n_words, R, n_bytes);
+    len_A1 = 2;
 
-                and(R, A + (i*n_words), B0, n_words);
-                pop = pop_count(R, n_words);
+    and(R, B0, C0, n_words);
+    memcpy(A0, R, n_bytes);
+    and(R, B1, C0, n_words);
+    memcpy(A0+n_words, R, n_bytes);
+    len_A0 = 2;
 
-                if (pop == 1) 
-                    S += 1;
-                else if (pop > 0) {
-                    memcpy(t_A+len_t_A*n_words, R, n_bytes);
-                    len_t_A += 1;
-                }
+    i = locus_i + 1;
+    e0 = 1;
+    e1 = 1;
+    //while ( ((linelen = getline(&line, &linecap, fp)) > 0) &&
+            //(i < locus_i + 150)) {
+    while ( ((linelen = getline(&line, &linecap, fp)) > 0) &&
+            (e0 > 0.05) && (e1 > 0.05) ) {
+        // skip some fields
+        p = strtok(line, " ");
+        p = strtok(NULL, " ");
+        p = strtok(NULL, " ");
+        p = strtok(NULL, " ");
+        h = p + strlen(p) + 1;
 
-                and(R, A + (i*n_words), B1, n_words);
-                pop = pop_count(R, n_words);
+        // parse the variants into bit arays 
+        B0 = B + (B_i*2*n_words);
+        B1 = B + ((B_i*2+1)*n_words);
+        set_bit_arrays(h, B0, B1, num_samples);
 
-                if (pop == 1) 
-                    S += 1;
-                else if (pop > 0) {
-                    memcpy(t_A+len_t_A*n_words, R, n_bytes);
-                    len_t_A += 1;
-                }
-            }
+        B_i += 1;
 
-            len_A = len_t_A;
-            swap_A = A;
-            A = t_A;
-            t_A = swap_A;
-        }
+        printf("%u\t%f\t", physical_pos[i]-locus_physical_pos,
+                           genetic_pos[i]-locus_genetic_pos);
+        e1 = ehh_step(&A1,
+                      &len_A1,
+                      C1,
+                      num_C1,
+                      B,
+                      i,
+                      &S,
+                      n_words,
+                      n_bytes,
+                      B0_c,
+                      B1_c,
+                      R,
+                      &t_A);
+
+        printf("%Lf\t", e1);
+
+        e0 = ehh_step(&A0,
+                      &len_A0,
+                      C0,
+                      num_C0,
+                      B,
+                      i,
+                      &S,
+                      n_words,
+                      n_bytes,
+                      B0_c,
+                      B1_c,
+                      R,
+                      &t_A);
+
+        printf("%Lf\n", e0);
+
+        i += 1;
     }
-    
-    uint32_t sum = 0;
-    for (i = 0; i < len_A; ++i) {
-        printf("%u\n", pop_count(A+(i*n_words), n_words));
-        sum += pop_count(A+(i*n_words), n_words);
-    }
-
-    sum += S;
-    printf("S:%u\n", S);
-    printf("sum:%u\n", sum);
-
-    free(line);
-    fclose(fp);
-#endif
 
     return 0;
 }
